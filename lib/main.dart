1af0771ffac500cb329e71fae26e7bdb966dc539
Lone Aadil <loneaadil4248@gmail.com>
2022-02-11 02:18:30 +0530

    fourth day

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
-
+import 'package:expansion_tile_card/expansion_tile_card.dart';
 import 'dart:ui';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(HorizontalList());
+  runApp(ExpansionTileCard1());
 }
 
 class GeekForGeeks extends StatelessWidget {
@@ -258,8 +258,38 @@ class HorizontalList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: title,
       home: Scaffold(
+        appBar: AppBar(
+          leading: IconButton(
+            icon: Icon(
+              Icons.menu,
+              color: Colors.orange,
+            ),
+            onPressed: () {},
+            tooltip: "i am menu",
+          ),
+          actions: [
+            IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.settings,
+                color: Colors.orange,
+              ),
+              tooltip: "i am settings",
+            ),
+          ],
+          elevation: 30.0,
+          shadowColor: Colors.blue,
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+          backgroundColor: Colors.green,
+          title: Text(
+            title,
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+        ),
         body: Container(
           padding: EdgeInsets.only(left: 20),
           margin: EdgeInsets.symmetric(vertical: 20.0),
@@ -295,19 +325,130 @@ class HorizontalList extends StatelessWidget {
               height: 480.0,
               width: 350.0,
               decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(30),
+                borderRadius: BorderRadius.circular(100),
                 image: DecorationImage(
                   image: NetworkImage(
-                      'https://wallpaperaccess.com/full/184117.jpg'),
+                    'https://wallpaperaccess.com/full/184117.jpg',
+                  ),
                 ),
                 shape: BoxShape.rectangle,
               ),
             )
           ]),
         ),
-        appBar: AppBar(
-          title: Text(title),
-        ),
+      ),
+    );
+  }
+}
+
+//Expansion tile
+class ExpansionTileCard1 extends StatelessWidget {
+  const ExpansionTileCard1({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'ExpansionTileCard',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: MyHomePage(title: 'Hello aadil'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final GlobalKey<ExpansionTileCardState> cardA = GlobalKey();
+  final GlobalKey<ExpansionTileCardState> cardB = GlobalKey();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+        shape:
+            RoundedRectangleBorder(borderRadius: BorderRadius.circular(25.0)),
+      ),
+      body: ListView(
+        padding: EdgeInsets.only(top: 10),
+        children: [
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 12.0),
+            child: ExpansionTileCard(
+              borderRadius: BorderRadius.circular(20),
+              baseColor: Colors.pink,
+              expandedColor: Colors.blue,
+              // animateTrailing: true,
+              // initiallyExpanded: true,
+              key: cardB,
+              leading: CircleAvatar(child: Icon(Icons.verified_user)),
+              title: Text('Tap to expand !'),
+              subtitle: Text('It has the CFG Logo1 .'),
+              children: [
+                Divider(
+                  color: Colors.blue,
+                  thickness: 12.0,
+                  height: 1.0,
+                ),
+                Align(
+                  alignment: Alignment.center,
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 16.0,
+                      vertical: 8.0,
+                    ),
+                    child: Image.asset('assets/images/car.jpg'),
+                  ),
+                )
+              ],
+            ),
+          ),
+          Divider(
+            thickness: 2.0,
+            height: 1.0,
+            color: Colors.lightBlue,
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 12.0),
+            child: ExpansionTileCard(
+              borderRadius: BorderRadius.circular(20),
+              baseColor: Colors.pink,
+              expandedColor: Colors.blue,
+              // animateTrailing: true,
+              key: cardA,
+              leading: CircleAvatar(
+                child: Icon(Icons.verified_user),
+              ),
+              title: Text('tap to expand 2'),
+              subtitle: Text('It has the cfg logo 2'),
+              children: [
+                Divider(
+                  color: Colors.blue,
+                  thickness: 12.0,
+                  height: 1.0,
+                ),
+                Align(
+                  alignment: Alignment.center,
+                  child: Padding(
+                    padding:
+                        EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+                        child: Image.asset('assets/images/lollipop.jpg')),
+                  
+                ),
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
